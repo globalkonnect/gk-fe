7257b0d1342b4499572b337f62732cf57a41b078
Jayshree sadangi <jayashreesadangi000@gmail.com>
2024-08-10 01:33:25 +0530

    add : comments

--- src/Pages/Clients.jsx
+++ src/Pages/Clients.jsx
@@ -8,7 +8,8 @@ function Clients() {
   const bajaj = allAssets.bajaj;
   const clientsImg = [tata, pandg, topcem, bajaj];
   return (
-    <div className="w-full flex items-center justify-center bg-Offwhite">
+    <div className="w-full flex flex-col items-center justify-center font-Rammetto text-3xl bg-Offwhite py-5">
+        <p>-Clients-</p>
       <div className="py-10 grid max-md:grid-cols-2 md:grid-cols-4 items-center justify-center w-[90vw]">
         {clientsImg.map((value,index)=>{return <span className="flex items-center justify-center" key={index}><ClientCard bgImg={value}/></span>}) }
       </div>
