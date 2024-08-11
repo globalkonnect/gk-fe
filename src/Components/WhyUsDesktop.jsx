df744b4ce5ae65b6bd30e2e13d965d24acc33bdf
Jayshree sadangi <jayashreesadangi000@gmail.com>
2024-08-11 11:55:10 +0530

    section:whyus

--- src/Components/WhyUsDesktop.jsx
+++ src/Components/WhyUsDesktop.jsx
@@ -3,7 +3,7 @@ import { allAssets } from "../Utility/baseAssets";
 import { globalKonnect } from "../Utility/data";
 function WhyUsDesktop() {
   const bentLine = allAssets.bentLine;
-  const data = globalKonnect.whyChooseGK.bullets
+  const data = globalKonnect.whyChooseGK.bullets;
   return (
     <div className="flex max-xl:hidden">
       {/* line */}
@@ -20,14 +20,19 @@ function WhyUsDesktop() {
               }        
             `}
             >
-              <p className={`bg-white size-[3rem] ${index + 1 == 2 ? "mt-[22px]" : "-mt-[0px]"} flex items-center justify-center rounded-full font-Rammetto text-2xl mr-5 text-Yellow`}>
+              <p
+                className={`bg-white size-[3rem] ${
+                  index + 1 == 2 ? "mt-[22px]" : "-mt-[0px]"
+                } flex items-center justify-center rounded-full font-Rammetto text-2xl mr-5 text-Yellow`}
+              >
                 {/* bullet index number */}
                 {index + 1}
               </p>
               {/* text value */}
               <p className="text-justify w-[35rem] ml-10 ">
-               <span className="text-2xl">{value.title}</span> <br />
-                {value.point}</p>
+                <span className="text-2xl">{value.title}</span> <br />
+                {value.point}
+              </p>
             </p>
           );
         })}
