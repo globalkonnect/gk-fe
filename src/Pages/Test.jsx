eec79055193c85d510c99cd7037f851c9a3f576b
jayshreee10 <2001109280_ee@pmec.ac.in>
2024-08-15 00:38:13 +0530

    add : swiper

---
+++ src/Pages/Test.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Test() {
+  return (
+    <div>
+        <canvas className='w-full h-[100vh] text-black'>
+       fff
+        </canvas>
+    </div>
+  )
+}
+
+export default Test
