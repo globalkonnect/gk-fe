a23a3e2831dcd2852e1da9d9ad73838f44240326
jayshreee10 <2001109280_ee@pmec.ac.in>
2024-08-20 15:16:03 +0530

    update: responsive in home component

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,7 @@ import { useEffect } from "react";
 import { Route, Routes, useLocation } from "react-router-dom";
 import Footer from "./Components/Footer";
 import Header from "./Components/Header";
-import About from "./Pages/About";
-import Clients from "./Pages/Clients";
-import ContactUs from "./Pages/ContactUs";
 import Home from "./Pages/Home";
-import Recognitions from "./Pages/Recognitions";
-import Testimonials from "./Pages/Testimonials";
-import Tours from "./Pages/Tours";
-import VisaServices from "./Pages/VisaServices";
-import WhyUs from "./Pages/WhyChoose";
-import Service from "./Pages/Service";
 function App() {
   const location = useLocation();
   useEffect(() => {
@@ -28,15 +19,15 @@ function App() {
           element={
             <div>
               <Home />
-              <About />
-              <Tours />
-              <WhyUs />
-              <Service />
-              <Clients />
-              <VisaServices />
-              <Recognitions />
-              <Testimonials />
-              <ContactUs />
+              {/* <About /> */}
+              {/* <Tours /> */}
+              {/* <WhyUs /> */}
+              {/* <Service /> */}
+              {/* <Clients /> */}
+              {/* <VisaServices /> */}
+              {/* <Recognitions /> */}
+              {/* <Testimonials /> */}
+              {/* <ContactUs /> */}
             </div>
           }
         ></Route>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -16,7 +16,7 @@ import { globalKonnect } from "../Utility/data";
 
 const Home = () => {
   const swiperRef = useRef();
-  const autoPlayDelayDuration = 2000;
+  const autoPlayDelayDuration = 1000;
   const [currentIndex, setCurrentIndex] = useState(0);
   const isTablet = useMediaQuery({ query: "(max-width:768px)" });
   const [direction, setDirection] = useState("up");
@@ -24,7 +24,16 @@ const Home = () => {
   const images = [imag1, imag2, imag3, imag4, imag5];
 
   const next = () => {
-    swiperRef.current.slideNext();
+    if (isTablet) {
+      const nextIndex = currentIndex + 1;
+      if (nextIndex >= images.length) {
+        setCurrentIndex(0);
+      } else {
+        setCurrentIndex(nextIndex);
+      }
+    } else {
+      swiperRef.current.slideNext();
+    }
   };
 
   const previous = () => {
@@ -36,8 +45,8 @@ const Home = () => {
       next();
     }, autoPlayDelayDuration);
 
-    return () => clearInterval(interval); // Clean up interval on component unmount
-  }, []);
+    return () => clearInterval(interval);
+  });
 
   return (
     <div
@@ -50,26 +59,34 @@ const Home = () => {
         direction={direction}
       />
 
-      <div className="absolute h-screen flex flex-col items-center justify-start pt-[25vh]">
-        <div className="w-[70vw] h-[150px] pb-48">
+      <div className="absolute h-screen flex flex-col items-center justify-start max-md:justify-center max-md:items-center pt-[25vh]">
+        <div className="w-[70vw] max-md:w-[90vw] h-[150px] pb-48 max-md:pb-0">
           <TextTransition
-            className="text-[120px] text-white font-Rammetto text-start w-[70vw]"
+            className="text-[10vh] max-md:text-[80px] max-sm:text-[55px] text-white font-Rammetto text-start max-md:text-center w-[70vw]"
             springConfig={presets.wobbly}
             direction={direction}
+            style={{
+              width: "100%",
+              height: "100%",
+              display: "flex",
+              alignItems: isTablet ? "center" : "flex-start",
+              justifyContent: isTablet ? "center" : "flex-start",
+              textAlign: isTablet ? "center" : "left",
+            }}
           >
             {homeData[currentIndex].title}
           </TextTransition>
         </div>
 
-        <div className="flex items-start justify-start h-[50vh] w-[100vw] ">
-          <div className="w-[50vw] flex items-end justify-start pl-[15vw]">
-            <div className="flex-col flex items-start justify-center w-[30vw] ">
-              <p className="text-xl w-[30vw] text-start">
+        <div className="flex items-start justify-start max-md:justify-center h-[50vh] w-[100vw] ">
+          <div className="w-[50vw] max-md:w-[80vw] flex items-end justify-start max-md:justify-center pl-[15vw] max-md:pl-0">
+            <div className="flex-col flex items-start justify-center max-md:items-center  w-[30vw] max-md:w-[80vw]">
+              <p className="h-[120px] max-md:h-[150px] text-xl max-md:text-[30px] max-sm:text-[25px] max-md:leading-9 w-[30vw] max-md:w-[60vw] max-sm:w-[80vw] text-start max-md:text-center max-md:pb-5 max-sm:pb-2 overflow-hidden">
                 {homeData[currentIndex].description}
               </p>
-              <p className="my-5 flex items-center justify-evenly tracking-[3px]">
+              <p className="my-5 flex items-start justify-start max-md:justify-center max-md:items-center flex-col tracking-[3px] max-md:text-[25px] max-sm:text-[18px] max-md:pl-10 max-sm:pl-0">
                 {homeData[currentIndex].tags}
-                <span className="mx-5">
+                <span className="pt-5">
                   <button
                     type="button"
                     className="px-6 py-2 bg-Yellow text-black rounded-3xl flex items-center justify-center"
@@ -104,9 +121,11 @@ const Home = () => {
               }}
               breakpoints={{
                 0: { slidesPerView: 1 },
-                611: { slidesPerView: 1.5 },
-                768: { slidesPerView: 1.5 },
-                1024: { slidesPerView: 1.5 },
+                611: { slidesPerView: 1 },
+                768: { slidesPerView: 1 },
+                1024: { slidesPerView: 1.2 },
+                1280: { slidesPerView: 1.5 },
+                1536: { slidesPerView: 1.8 },
               }}
             >
               {images.map((value, index) => {
@@ -115,7 +134,7 @@ const Home = () => {
                   <SwiperSlide key={index}>
                     <img
                       src={value}
-                      className="h-[400px] w-[550px] object-cover rounded-[32px]"
+                      className="h-[400px] lg:w-[550px] w-[450px] max-lg:w-[400px] object-cover rounded-[32px]"
                       onClick={() => {
                         if (!isCurrent) next();
                       }}
@@ -134,22 +153,3 @@ const Home = () => {
 };
 
 export default Home;
-
-{
-  /* 
-  
-  <button
-onClick={previous}
-className="px-4 py-2 bg-gray-800 text-white rounded"
->
-Previous
-</button>
-<button
-onClick={next}
-className="px-4 py-2 bg-gray-800 text-white rounded"
->
-Next
-</button> 
-
-*/
-}
