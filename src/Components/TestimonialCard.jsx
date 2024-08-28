7a0faf21c8cac3d7901ffcfa2489df75d585d129
jayshreee10 <2001109280_ee@pmec.ac.in>
2024-08-28 21:52:55 +0530

    fix:testimonial card responsiveness

--- src/Components/TestimonialCard.jsx
+++ src/Components/TestimonialCard.jsx
@@ -35,7 +35,11 @@ function TestimonialCard() {
           className="swiper-button-prev md:hidden flex items-center justify-center cursor-pointer rounded-full"
           onClick={() => swiperRef.current?.slidePrev()}
         >
-          <img src={arrowLeft} alt="prev" className="h-12 max-md:h-10 max-sm:h-[30px]" />
+          <img
+            src={arrowLeft}
+            alt="prev"
+            className="h-12 max-md:h-10 max-sm:h-[30px]"
+          />
         </div>
         <div className="w-[80vw] flex items-center justify-evenly md:hidden font-Alata text-lg">
           <Swiper
@@ -43,22 +47,36 @@ function TestimonialCard() {
             loop={true}
             autoplay={{ delay: 5000 }}
             speed={3000}
-            slidesOffsetBefore={isMobile ? 40 : 50}
+            slidesOffsetAfter={isMobile ? 10 : 50}
             centeredSlides={true}
             centeredSlidesBounds={true}
             onSwiper={(swiper) => (swiperRef.current = swiper)}
             breakpoints={{
-              0: { slidesPerView: 1 },
-              310: { slidesPerView: 0.75 },
-              350: { slidesPerView: 0.8 },
-              370: { slidesPerView: 1 },
-              440: { slidesPerView: 1.4 },
-              455: { slidesPerView: 1.5 },
+              0: { slidesPerView: 1, spaceBetween: 20 },
+              320: { slidesPerView: 1.1, spaceBetween: 20 },
+              335: { slidesPerView: 1.1, spaceBetween: 20 },
+              360: { slidesPerView: 1.2, spaceBetween: 20 },
+              370: { slidesPerView: 1.23, spaceBetween: 20 },
+              380: { slidesPerView: 1.25, spaceBetween: 20 },
+              390: { slidesPerView: 1.28, spaceBetween: 20 },
+              400: { slidesPerView: 1.3, spaceBetween: 20 },
+              410: { slidesPerView: 1.4, spaceBetween: 20 },
+              460: { slidesPerView: 1.5, spaceBetween: 20 },
+              480: { slidesPerView: 1.55, spaceBetween: 20 },
+              530: { slidesPerView: 1.6, spaceBetween: 20 },
+              560: { slidesPerView: 1.7, spaceBetween: 20 },
+              590: { slidesPerView: 1.8, spaceBetween: 20 },
+              600: { slidesPerView: 2, spaceBetween: 30 },
+              640: { slidesPerView: 1.5, spaceBetween: 30 },
+              660: { slidesPerView: 1.6, spaceBetween: 30 },
+              720: { slidesPerView: 1.7, spaceBetween: 30 },
+              768: { slidesPerView: 2, spaceBetween: 40 },
+              1024: { slidesPerView: 3, spaceBetween: 50 },
             }}
           >
             {testimonials.map((value, index) => (
               <SwiperSlide key={index}>
-                <div className="h-[20rem] flex-col max-sm:h-[13rem] max-sm:w-[15rem] sm:w-[20rem] md:w-[30rem] max-sm:text-xs my-10 p-5 bg-white rounded-3xl flex items-center justify-center">
+                <div className="h-[20rem] flex-col max-sm:h-[13rem] max-sm:w-[240px]  sm:w-[20rem] md:w-[30rem] max-sm:text-xs my-10 p-5 bg-white rounded-3xl flex items-center justify-center">
                   <p className="text-center">{value.text}</p>
                   <div className="mt-2 text-end">- {value.people} -</div>
                 </div>
@@ -70,7 +88,11 @@ function TestimonialCard() {
           className="swiper-button-next md:hidden flex items-center justify-center cursor-pointer rounded-full"
           onClick={() => swiperRef.current?.slideNext()}
         >
-          <img src={arrowRight} alt="next" className="h-12 max-md:h-10 max-sm:h-[30px]" />
+          <img
+            src={arrowRight}
+            alt="next"
+            className="h-12 max-md:h-10 max-sm:h-[30px]"
+          />
         </div>
       </div>
     </div>
