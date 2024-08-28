c05169ab354f3853bd0c2d5419a8a2f7eaf1924d
jayshreee10 <2001109280_ee@pmec.ac.in>
2024-08-28 19:04:49 +0530

    change path

--- src/Utility/baseAssets.js
+++ src/Utility/baseAssets.js
@@ -1,77 +1,77 @@
 //  Recognition
-import verticalLine from "../assets/Recognition/lineVertical.png";
-import dubai from "../assets/Recognition/dubai.png";
-import ministryOfTourism from "../assets/Recognition/ministryofTourism.png";
-import awesomeAssam from "../assets/Recognition/awesomeAssam.png";
+import verticalLine from "@/assets/Recognition/lineVertical.png";
+import dubai from "@/assets/Recognition/dubai.png";
+import ministryOfTourism from "@/assets/Recognition/ministryofTourism.png";
+import awesomeAssam from "@/assets/Recognition/awesomeAssam.png";
 
 //Clients
-import tata from "../assets/Clients/Tata.png";
-import pandg from "../assets/Clients/PandG.png";
-import topcem from "../assets/Clients/Topcem.png";
-import bajaj from "../assets/Clients/Bajaj.png";
+import tata from "@/assets/Clients/Tata.png";
+import pandg from "@/assets/Clients/PandG.png";
+import topcem from "@/assets/Clients/Topcem.png";
+import bajaj from "@/assets/Clients/Bajaj.png";
 
 //Aboutus
-import aboutImg from "../assets/AboutUs/About.png";
+import aboutImg from "@/assets/AboutUs/About.png";
 
 //whyus
-import bentLine from "../assets/whyUs/bentLine.png"
-import rectangle from "../assets/whyUs/Rectangle.png"
+import bentLine from "@/assets/whyUs/bentLine.png"
+import rectangle from "@/assets/whyUs/Rectangle.png"
 
 //Footer
-import Line from "../assets/Others/Line.png"
+import Line from "@/assets/Others/Line.png"
 
 // Visa Services
-import visaImg1 from "../assets/VisaService/visaimg1.png"
-import visaImg2 from "../assets/VisaService/visaimg2.png"
-import visaImg3 from "../assets/VisaService/visaimg3.png"
+import visaImg1 from "@/assets/VisaService/visaimg1.png"
+import visaImg2 from "@/assets/VisaService/visaimg2.png"
+import visaImg3 from "@/assets/VisaService/visaimg3.png"
 
 //Services
-import airplane from '../assets/Services/airplane.png'
-import change from '../assets/Services/change.png'
-import honeymoon from '../assets/Services/honeymoon.png'
-import visa from '../assets/Services/visa.png'
-import bg from '../assets/Services/ServiceBackground.webp'
+import airplane from '@/assets/Services/airplane.png'
+import change from '@/assets/Services/change.png'
+import honeymoon from '@/assets/Services/honeymoon.png'
+import visa from '@/assets/Services/visa.png'
+import bg from '@/assets/Services/ServiceBackground.webp'
 //Tours
-import northEI from "../assets/Tours/NEI.png"
-import Thailand from "../assets/Tours/Thailand.png"
-import Dubai from "../assets/Tours/Dubai.png"
+import northEI from "@/assets/Tours/NEI.png"
+import Thailand from "@/assets/Tours/Thailand.png"
+import Dubai from "@/assets/Tours/Dubai.png"
 //Testimonial
-import bgTestimonial from "../assets/Testimonial/testimonialbg.png"
+import bgTestimonial from "@/assets/Testimonial/testimonialbg.png"
 //header
-import GK from "../assets/Others/logo.svg"
+import GK from "@/assets/Others/logo.svg"
 //others
-import arrowLeft from "../assets/Others/leftarrow.png" 
-import arrowRight from "../assets/Others/rightarrow.png"
+import arrowLeft from "@/assets/Others/leftarrow.png" 
+import arrowRight from "@/assets/Others/rightarrow.png"
 
 //home 
-import NEIbg from "../assets/Home/NorthEastIndiaDesktop.webp"
-import Dubaibg from "../assets/Home/Dubaidesktop.webp"
-import Singaporebg from "../assets/Home/Singaporedesktop.webp"
-import Thailandbg from "../assets/Home/Thailanddesktop.webp"
-import Balibg from "../assets/Home/Balidesktop.webp"
-import Homeimg1 from "../assets/Home/Homeimg1.webp"
-import Homeimg2 from "../assets/Home/Homeimg2.webp"
-import Homeimg3 from "../assets/Home/Homeimg3.webp"
-import Homeimg4 from "../assets/Home/Homeimg4.webp"
-import Homeimg5 from "../assets/Home/Homeimg5.webp"
-import Homeimg6 from "../assets/Home/Homeimg6.webp"
-import Homeimg7 from "../assets/Home/Homeimg7.webp"
-import Homeimg8 from "../assets/Home/Homeimg8.webp"
-import Homeimg9 from "../assets/Home/Homeimg9.webp"
-import Homeimg10 from "../assets/Home/Homeimg10.webp"
+import NEIbg from "@/assets/Home/NorthEastIndiaDesktop.webp"
+import Dubaibg from "@/assets/Home/Dubaidesktop.webp"
+import Singaporebg from "@/assets/Home/Singaporedesktop.webp"
+import Thailandbg from "@/assets/Home/Thailanddesktop.webp"
+import Balibg from "@/assets/Home/Balidesktop.webp"
+import Homeimg1 from "@/assets/Home/Homeimg1.webp"
+import Homeimg2 from "@/assets/Home/Homeimg2.webp"
+import Homeimg3 from "@/assets/Home/Homeimg3.webp"
+import Homeimg4 from "@/assets/Home/Homeimg4.webp"
+import Homeimg5 from "@/assets/Home/Homeimg5.webp"
+import Homeimg6 from "@/assets/Home/Homeimg6.webp"
+import Homeimg7 from "@/assets/Home/Homeimg7.webp"
+import Homeimg8 from "@/assets/Home/Homeimg8.webp"
+import Homeimg9 from "@/assets/Home/Homeimg9.webp"
+import Homeimg10 from "@/assets/Home/Homeimg10.webp"
 //aestheticimages
-import img1 from "../assets/Others/aeroplaneicon.png"
-import img2 from "../assets/Others/img2.svg"
-import img3 from "../assets/Others/img3.svg"
-import img4 from "../assets/Others/img4.svg"
-import img5 from "../assets/Others/testimonialaesthatic.svg"
+import img1 from "@/assets/Others/aeroplaneicon.png"
+import img2 from "@/assets/Others/img2.svg"
+import img3 from "@/assets/Others/img3.svg"
+import img4 from "@/assets/Others/img4.svg"
+import img5 from "@/assets/Others/testimonialaesthatic.svg"
 ////aesthetic mobile
-import img6 from "../assets/Others/img6.svg"
-import img7 from "../assets/Others/img7.svg"
-import img8 from "../assets/Others/img8.svg"
-import img9 from "../assets/Others/img9.svg"
-import img10 from "../assets/Others/testimonyimg.svg"
-import img11 from "../assets/Others/img11.svg"
+import img6 from "@/assets/Others/img6.svg"
+import img7 from "@/assets/Others/img7.svg"
+import img8 from "@/assets/Others/img8.svg"
+import img9 from "@/assets/Others/img9.svg"
+import img10 from "@/assets/Others/testimonyimg.svg"
+import img11 from "@/assets/Others/img11.svg"
 
 export const allAssets = {
 
