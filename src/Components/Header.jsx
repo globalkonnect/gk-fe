6dda09c4419daf8c3dcae2187df7a7c47122d08a
jayshreee10 <2001109280_ee@pmec.ac.in>
2024-08-29 20:37:18 +0530

    contact us btn ui update

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -20,8 +20,15 @@ function Header() {
   return (
     <div className="fixed top-0 md:left-[15vw] max-md:left-0 z-20 bg-offBlack bg-opacity-80 flex items-center justify-between md:w-[70vw] max-md:w-full font-Alata py-3 rounded-b-3xl shadow-lg cursor-pointer">
       <div className="ml-10 flex items-center justify-center">
-        <img src={GK} alt="Logo" onClick={()=>{navigate("/#home")}} title="Global Konnect" 
-        className="max-md:size-12"/>
+        <img
+          src={GK}
+          alt="Logo"
+          onClick={() => {
+            navigate("/#home");
+          }}
+          title="Global Konnect"
+          className="max-md:size-12"
+        />
       </div>
 
       <button
@@ -51,7 +58,7 @@ function Header() {
           onClick={() => {
             navigate("/#contactus");
           }}
-          className="px-3 py-1 bg-Yellow text-offBlack flex items-center justify-center rounded-3xl cursor-pointer"
+          className="px-3 py-1 bg-Yellow hover:bg-white  text-offBlack flex items-center justify-center rounded-3xl cursor-pointer"
         >
           CONTACT US
         </button>
